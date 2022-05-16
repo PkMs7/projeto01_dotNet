9d0ca8f2333163f207baf2e532d442f3db589a28
Patrick Marques <patrickmarques2009@hotmail.com>
2022-05-16 13:10:45 -0300

    Inicialização do projeto

---
+++ projetoAlunos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projetoAlunos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
